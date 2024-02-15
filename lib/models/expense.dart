9b1a5fd04627adeac5f4ddc124f0e2e60c5ae2cc
Srvictorchinea <sr.victorchinea@gmail.com>
2024-02-15 22:47:10 +0000

    [ADD] category icons

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
   final String id;
@@ -11,5 +19,11 @@ class Expense {
   final double amount;
   final DateTime date;
 
-  Expense({ required this.title, required this.category, required this.amount, required this.date}) : id = uuid.v4();
+  Expense(
+      {required this.title,
+      required this.category,
+      required this.amount,
+      required this.date})
+      : id = uuid.v4();
+      
 }
--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -19,10 +19,18 @@ class ExpenseItem extends StatelessWidget {
         child: Column(
           children: [
             Text(expense.title),
-            const SizedBox(height: 4,)
+            const SizedBox(height: 4,),
             Row(
               children: [
                 Text('${expense.amount.toStringAsFixed(2)} €'),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(Icons.alarm),
+                    const SizedBox(width: 8,),
+                    Text(expense.date.toString()),
+                  ],
+                )
               ],
             )
           ],
