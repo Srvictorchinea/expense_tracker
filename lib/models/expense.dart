9b1918088eff5f88845e93af679b876d567a4c0a
Srvictorchinea <sr.victorchinea@gmail.com>
2024-02-15 12:46:44 +0000

    [NEW] expesive model

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,15 +1,15 @@
 import 'package:uuid/uuid.dart';
 
-final uuid = Uuid();
+const uuid = Uuid();
+
+enum Category { food, travel, leisure, work }
 
 class Expense {
-
   final String id;
   final String title;
+  final String category;
   final double amount;
   final DateTime date;
 
-  Expense(this.title, this.amount, this.date) : id = uuid.v4();
-
-
+  Expense(this.title, this.category, this.amount, this.date) : id = uuid.v4();
 }
