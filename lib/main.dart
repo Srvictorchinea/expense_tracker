26dd94cd39651ff4edf1387276c89890bc4da1bb
Srvictorchinea <sr.victorchinea@gmail.com>
2024-02-18 01:24:24 +0000

    [ADD] themes styles from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
+
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(245, 244, 244, 244));
+
 void main() {
-  runApp( MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Expenses(), 
-  ),
-  
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 14),
+            ),
+      ),
+      home: const Expenses(),
+    ),
   );
 }
-
